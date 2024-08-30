944b484e265f9b7083bfeead2d4c06ec8e2f807d
dmu110930 <dmu110930@stud.gibb.ch>
2024-08-30 12:54:34 +0200

    added m,aui

---
+++ Prog1/Prog1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Prog1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
